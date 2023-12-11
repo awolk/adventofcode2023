d8d08fd4cc20a0e22e894df0c2b6f294c9f21f9b
Aidan Wolk <aidanwolk@comcast.net>
2023-12-10 21:37:36 -0800

    Implement day 11 part 2

--- ruby/day11.rb
+++ ruby/day11.rb
@@ -1,27 +1,35 @@
 require_relative './lib/aoc'
 
-def expand_rows(grid)
-  res_rows = []
-  grid.to_a.each do |row|
-    res_rows << row
-    res_rows << row if row.all?('.')
+class Image
+  def initialize(grid)
+    @empty_rows = (0 ... grid.row_count).select do |row_index|
+      grid[row_index, 0..].all?('.')
+    end.to_set
+    @empty_cols = (0 ... grid.column_count).select do |column_index|
+      (0...grid.row_count).all? {|r| grid[r, column_index] == '.'}
+    end.to_set
+    @galaxies = grid.each_with_index.filter_map do |val, r, c|
+      [r, c] if val == '#'
+    end
   end
-  Matrix.rows(res_rows)
+
+  def sum_path_lengths(expansion)
+    @galaxies.combination(2).sum do |((r1, c1), (r2, c2))|
+      r1, r2 = [r1, r2].sort
+      c1, c2 = [c1, c2].sort
+      
+      row_dist = r2 - r1 + (expansion - 1) * (@empty_rows & (r1 .. r2)).size
+      col_dist = c2 - c1 + (expansion - 1) * (@empty_cols & (c1 .. c2)).size
+      row_dist + col_dist
+    end
+  end
 end
 
 input = AOC.get_input(11)
-grid = AOC.char_matrix(input)
-grid = expand_rows(grid)
-grid = expand_rows(grid.transpose).transpose
+image = Image.new(AOC.char_matrix(input))
 
-positions = grid.each_with_index.filter_map do |val, r, c|
-  [r, c] if val == '#'
-end
-
-pt1 = positions.combination(2).sum do |((r1, c1), (r2, c2))|
-  (r1 - r2).abs + (c1 - c2).abs
-end
-
+pt1 = image.sum_path_lengths(2)
 puts "Part 1: #{pt1}"
-pt2 = 0
+
+pt2 = image.sum_path_lengths(1000000)
 puts "Part 2: #{pt2}"
