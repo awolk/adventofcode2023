019859b3f6efbb7ffbf6a1186e4e1907078ce877
Aidan Wolk <aidanwolk@comcast.net>
2023-12-16 22:53:35 -0800

    Day 17 consolidate parts

--- ruby/day17.rb
+++ ruby/day17.rb
@@ -9,8 +9,10 @@ def diff(pos1, pos2)
 end
 
 class Graph
-  def initialize(grid)
+  def initialize(grid, min_steps_after_turn, max_steps_in_same_dir)
     @grid = grid
+    @min_steps_after_turn = min_steps_after_turn
+    @max_steps_in_same_dir = max_steps_in_same_dir
     @goal = [grid.row_count - 1, grid.column_count - 1]
   end
 
@@ -22,38 +24,19 @@ class Graph
     (r, c), = state
     @grid.row_count - r + @grid.column_count - c - 2
   end
-end
 
-class Part1Graph < Graph
-  def neighbors_with_cost(state)
-    pos, last_n_steps, last_dir = state
-    @grid.neighbors_with_positions(pos, diagonals: false).filter_map do |cost, neighbor_pos|
-      dir = diff(neighbor_pos, pos)
-      next if dir[0] == -last_dir[0] && dir[1] == -last_dir[1] # no turning backwards
-      if dir == last_dir
-        next if last_n_steps == 3
-        steps = last_n_steps + 1
-      else
-        steps = 1
-      end
-      [[neighbor_pos, steps, dir], cost]
-    end
-  end
-end
-
-class Part2Graph < Graph
   def neighbors_with_cost(state)
     pos, last_n_steps, last_dir = state
     dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
     dirs.delete([-last_dir[0], -last_dir[1]])
-    dirs.delete(last_dir) if last_n_steps == 10
+    dirs.delete(last_dir) if last_n_steps == @max_steps_in_same_dir
     dirs.filter_map do |dir|
       if dir != last_dir
-        # we turned, so go minimum 4 steps
-        new_pos = [pos[0] + 4*dir[0], pos[1] + 4*dir[1]]
+        # we turned, so go minimum inital steps
+        new_pos = [pos[0] + @min_steps_after_turn*dir[0], pos[1] + @min_steps_after_turn*dir[1]]
         next nil if !@grid.valid_pos?(new_pos)
-        cost = (1..4).sum {|n| @grid[pos[0] + n*dir[0], pos[1] + n*dir[1]]}
-        [[new_pos, 4, dir], cost]
+        cost = (1..@min_steps_after_turn).sum {|n| @grid[pos[0] + n*dir[0], pos[1] + n*dir[1]]}
+        [[new_pos, @min_steps_after_turn, dir], cost]
       else
         new_pos = [pos[0] + dir[0], pos[1] + dir[1]]
         next nil if !@grid.valid_pos?(new_pos)
@@ -93,8 +76,8 @@ def a_star(start, graph)
   raise "could not reach goal"
 end
 
-pt1 = a_star([[0, 0], 0, [0, 0]], Part1Graph.new(grid))
+pt1 = a_star([[0, 0], 0, [0, 0]], Graph.new(grid, 1, 3))
 puts "Part 1: #{pt1}"
 
-pt2 = a_star([[0, 0], 0, [0, 0]], Part2Graph.new(grid))
+pt2 = a_star([[0, 0], 0, [0, 0]], Graph.new(grid, 4, 10))
 puts "Part 2: #{pt2}"
