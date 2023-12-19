25395be9c7950e97bec5f63f70651d8495705a0b
Aidan Wolk <aidanwolk@comcast.net>
2023-12-18 21:55:07 -0800

    Implement day 19 part 2

--- ruby/day19.rb
+++ ruby/day19.rb
@@ -17,19 +17,18 @@ Workflow = Struct.new(:name, :rules, :fallback) do
   end
 end
 
-rule_parser = P.seq(P.word, P.regexp(/[<>]/).map(&:to_sym), P.int, ':', P.word).map do |category, op, threshold, destination|
-  Rule.new(category, op, threshold, destination)
+rule_parser = P.seq(P.word, P.regexp(/[<>]/).map(&:to_sym), P.int, ':', P.word).map do |args|
+  Rule.new(*args)
 end
-workflow_parser = P.seq(P.word, '{', rule_parser.delimited(','), ',', P.word, '}').map do |name, rules, fallback|
-  Workflow.new(name, rules, fallback)
+workflow_parser = P.seq(P.word, '{', rule_parser.delimited(','), ',', P.word, '}').map do |args|
+  Workflow.new(*args)
 end
 part_parser = P.str("{") >> P.seq(P.word, '=', P.int).delimited(',').map(&:to_h) << "}"
 parser = P.seq(workflow_parser.each_line, "\n\n", part_parser.each_line)
 
 input = AOC.get_input(19)
-# input = AOC.get_example_input(19)
 workflows, parts = parser.parse_all(input)
-workflows_map = workflows.map {[_1.name, _1]}.to_h
+workflows_map = workflows.to_h {[_1.name, _1]}
 
 pt1 = parts.sum do |part|
   workflow = 'in'
@@ -40,5 +39,47 @@ pt1 = parts.sum do |part|
 end
 puts "Part 1: #{pt1}"
 
-pt2 = 0
+
+# go through every workflow path possible, and then for each path determine the possible ranges of values
+# returns a list of lists of [category, range] for accepted conditions
+def paths(position, workflows_map)
+  return [] if position == 'R'
+  return [[]] if position == 'A'
+
+  res = []
+
+  workflow = workflows_map[position]
+  inverted_conditions = []
+  workflow.rules.each do |rule|
+    if rule.op == :<
+      range = 1 ... rule.threshold
+      inverse_range = rule.threshold .. 4000
+    elsif rule.op == :>
+      range = (rule.threshold + 1) .. 4000
+      inverse_range = 1 .. rule.threshold
+    end
+    cond = [rule.category, range]
+    results_for_dest = paths(rule.destination, workflows_map)
+    results_for_dest.each do |result_for_dest|
+      res << (result_for_dest + inverted_conditions + [cond])
+    end
+    inverted_conditions << [rule.category, inverse_range]
+  end
+
+  results_for_fallback = paths(workflow.fallback, workflows_map)
+  results_for_fallback.each do |result_for_fallback|
+    res << result_for_fallback + inverted_conditions
+  end
+
+  res
+end
+
+all_paths = paths('in', workflows_map)
+pt2 = all_paths.sum do |path|
+  xs = path.filter_map {|char, range| range if char == 'x'}.map(&:to_a).reduce(&:&)&.size || 4000
+  ms = path.filter_map {|char, range| range if char == 'm'}.map(&:to_a).reduce(&:&)&.size || 4000
+  as = path.filter_map {|char, range| range if char == 'a'}.map(&:to_a).reduce(&:&)&.size || 4000
+  ss = path.filter_map {|char, range| range if char == 's'}.map(&:to_a).reduce(&:&)&.size || 4000
+  xs * ms * as * ss
+end
 puts "Part 2: #{pt2}"
