86e074c4da0b1a94d334cd818c571d8840ef8e8d
Aidan Wolk <aidanwolk@comcast.net>
2023-12-28 11:23:23 -0800

    Implement day 24 part 2

--- ruby/day24.rb
+++ ruby/day24.rb
@@ -1,3 +1,5 @@
+require 'tempfile'
+require 'open3'
 require_relative './lib/aoc'
 require_relative './lib/parser'
 
@@ -21,7 +23,6 @@ stone_p = P.seq(vec_p, /\s*@\s*/, vec_p).map {|pos, _, vel| Stone.new(pos, vel)}
 stones_p = stone_p.each_line
 
 input = AOC.get_input(24)
-# input = AOC.get_example_input(24)
 stones = stones_p.parse_all(input)
 
 range = 200000000000000 .. 400000000000000
@@ -36,5 +37,32 @@ pt1 = stones.combination(2).count do |stone_a, stone_b|
 end
 puts "Part 1: #{pt1}"
 
-pt2 = 0
+# Use Z3 to solve part 2
+z3_in = Tempfile.new
+z3_in.write <<~Z3
+  (declare-const sx Int)
+  (declare-const sy Int)
+  (declare-const sz Int)
+  (declare-const svx Int)
+  (declare-const svy Int)
+  (declare-const svz Int)
+Z3
+stones.each_with_index do |stone, i|
+  z3_in.write <<~Z3
+    (declare-const t#{i} Int)
+    (assert (= (+ sx (* t#{i} svx)) (+ #{stone.pos[0].to_i} (* #{stone.vel[0].to_i} t#{i}))))
+    (assert (= (+ sy (* t#{i} svy)) (+ #{stone.pos[1].to_i} (* #{stone.vel[1].to_i} t#{i}))))
+    (assert (= (+ sz (* t#{i} svz)) (+ #{stone.pos[2].to_i} (* #{stone.vel[2].to_i} t#{i}))))
+  Z3
+end
+z3_in.write <<~Z3
+  (check-sat)
+  (eval (+ sx sy sz))
+Z3
+
+z3_in.close
+output = `z3 #{z3_in.path}`
+z3_in.unlink
+
+raise "invalid output" unless /sat\n(?<pt2>\d+)/ =~ output
 puts "Part 2: #{pt2}"
